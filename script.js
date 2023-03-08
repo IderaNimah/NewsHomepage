717df8683eddb8d2dc9ee7a852ea7e4711a130f0
IderaNimah <iderayusuf29@gmail.com>
2023-03-08 12:42:27 +0100

    My navbar is not working *tears

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const burger = document.querySelector(".burger");
-const navmenu = document.querySelector(".Navbar");
-const close = document.querySelector(".close");
+// const burger = document.querySelector(".burger");
+// const navmenu = document.querySelector(".Navbar");
+// const close = document.querySelector(".close");
 
-burger.addEventListener("click", () =>{
-    navmenu.classList.add("active");
-})
-close.addEventListener("click", () =>{
-    navmenu.classList.remove("active");
-})
+// burger.addEventListener("click", () =>{
+//     navmenu.classList.add("active");
+// })
+// close.addEventListener("click", () =>{
+//     navmenu.classList.remove("active");
+// })
+const harmburger = document.querySelector(".menu_bar");
+const navBar = document.querySelector(".navBar");
+
+// Adding and removing the class
+function toggleBar(){
+    navBar.classList.toggle("open");
+}
+harmburger.addEventListener('click',toggleBar)
