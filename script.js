4fa1ea9deae7e4905991378f14d89eb9aeb54434
IderaNimah <iderayusuf29@gmail.com>
2023-03-09 15:32:53 +0100

    navbar fixed

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// const burger = document.querySelector(".burger");
-// const navmenu = document.querySelector(".Navbar");
-// const close = document.querySelector(".close");
-
-// burger.addEventListener("click", () =>{
-//     navmenu.classList.add("active");
-// })
-// close.addEventListener("click", () =>{
-//     navmenu.classList.remove("active");
-// })
 const harmburger = document.querySelector(".menu_bar");
 const navBar = document.querySelector(".navBar");
 
